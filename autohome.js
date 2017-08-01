d23556fb507f4f308541ee8c5f8e199bb9c5bd43
Littlepunk <dave@littlepunk.net>
2017-08-01 22:38:39 +1200

    Latest

--- autohome.js
+++ autohome.js
@@ -15,7 +15,7 @@ var sp = new serialport.SerialPort(portName,{
 // helper function to get a nicely formatted date string
 function getDateString() {
     var time = new Date().getTime();
-    // 32400000 is (GMT+9 Japan)
+    // 32400000 is (GMT+9 Japan) 
     // for your timezone just multiply +/-GMT by 36000000
     var datestr = new Date(time +32400000).toISOString().replace(/T/, ' ').replace(/Z/, '');
     return datestr;
